d842d9714bca1a67e7d5fed6dbeee3d6cd0e5960
lazywon <jinjooisme@gmail.com>
2022-07-21 22:33:54 +0900

    feat: refactor - separate logic from App Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
+import HabitPresenter from './habit_presenter';
 
+const habitPresenter = new HabitPresenter([
+  { id: 1, name: 'Reading', count: 0 },
+  { id: 2, name: 'Running', count: 0 },
+  { id: 3, name: 'Coding', count: 0 },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App presenter={habitPresenter} />
+  </React.StrictMode>,
 );
-
