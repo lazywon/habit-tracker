d24e8a860bbf4aff1c22b24cf84688c560eadaa4
lazywon <jinjooisme@gmail.com>
2022-07-24 16:13:48 +0900

    feat: add sanpshot test for HabitAddForm

--- src/components/form/tests/habitAddForm.test.js
+++ src/components/form/tests/habitAddForm.test.js
@@ -2,41 +2,51 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HabitAddForm from '../habitAddForm';
+import renderer from 'react-test-renderer';
 
 describe('HabitAddForm', () => {
-  let onAdd;
-  let input;
-  let button;
+  it('renders', () => {
+    // 스냅샷 테스트를 실행
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
 
-  beforeEach(() => {
-    onAdd = jest.fn();
-    render(<HabitAddForm onAdd={onAdd} />);
-    input = screen.getByPlaceholderText('Enter Habit');
-    button = screen.getByText('Add');
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it('calls onAdd when button is clicked and valid habit is entered', () => {
-    // given
-    // render(<HabitAddForm onAdd={onAdd} />);
-    // const input = screen.getByPlaceholderText('Enter Habit');
-    // const button = screen.getByText('Add');
+  describe('Form Submit', () => {
+    let onAdd;
+    let input;
+    let button;
 
-    // when
-    // input에다 원하는 습관 이름을 타이핑 한 다음
-    // add 라는 버튼 클릭하면
-    userEvent.type(input, 'New Habit');
-    userEvent.click(button);
+    beforeEach(() => {
+      onAdd = jest.fn();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText('Enter Habit');
+      button = screen.getByText('Add');
+    });
 
-    // then
-    // onAdd가 input에 입력된 이름과 함께 호출된다.
-    expect(onAdd).toHaveBeenCalledWith('New Habit');
-  });
+    it('calls onAdd when button is clicked and valid habit is entered', () => {
+      // given
+      // render(<HabitAddForm onAdd={onAdd} />);
+      // const input = screen.getByPlaceholderText('Enter Habit');
+      // const button = screen.getByText('Add');
 
-  it('does not call onAdd when the habit is empty', () => {
-    // userEvent.type(input, '');
-    userEvent.clear(input);
-    userEvent.click(button);
+      // when
+      // input에다 원하는 습관 이름을 타이핑 한 다음
+      // add 라는 버튼 클릭하면
+      userEvent.type(input, 'New Habit');
+      userEvent.click(button);
 
-    expect(onAdd).toHaveBeenCalledTimes(0);
+      // then
+      // onAdd가 input에 입력된 이름과 함께 호출된다.
+      expect(onAdd).toHaveBeenCalledWith('New Habit');
+    });
+
+    it('does not call onAdd when the habit is empty', () => {
+      // userEvent.type(input, '');
+      userEvent.clear(input);
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
   });
 });
